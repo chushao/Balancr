591808900d360091451293f944ad8a9aed8654c6
Chu Shao <cshao@churenshao.com>
2014-02-15 19:30:40 -0800

    Set up registration

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var User = mongoose.model('User');
 // TODO remove later as this resets the database everytime. 
 User.remove().exec();
 //Seed the database
-
+/**
 var chu_data = {
 	"id" : 0,
 	"email" : "c@shao.com",
@@ -94,7 +94,7 @@ chu.save( function(error, data) {
 			}
 		});
 }
-});
+}); **/
 //facebook authentication
 var authids = require('./auth.js');
 var passport = require('passport');
@@ -257,7 +257,11 @@ app.get('/workplay', ensureAuthenticated, function(req, res){
 		calculate(0);
 		workPercent = Math.round( ((work / (work + play)) * 100) * 100) / 100;
 		playPercent = Math.round( ((play / (work + play)) * 100) * 100) / 100; 
-		res.render('workplay', {pageData: {workPercent: workPercent, playPercent: playPercent }});
+		var workGraph = isNaN(workPercent) ? 50 : workPercent;
+		var playGraph = isNaN(playPercent) ? 50 : playPercent;
+		if (isNaN(workPercent)) { workPercent = 0;}
+		if (isNaN(playPercent)) { playPercent = 0;}
+		res.render('workplay', {pageData: {workGraph: workGraph, playGraph: playGraph, workPercent: workPercent, playPercent: playPercent }});
 	});
 
 	
@@ -306,6 +310,15 @@ app.post('/add', ensureAuthenticated, function(req, res) {
 app.get('/calendar', ensureAuthenticated, routes.calendar);
 app.get('/details', ensureAuthenticated, routes.details);
 app.get('/signup', routes.signup);
+app.post('/signup', function (req, res, next) {
+		User.signup(req.body.email, req.body.password, function(err, user){
+			if(err) throw err;
+			req.login(user, function(err){
+				if(err) return next(err);
+				return res.redirect("/workplay");
+			});
+		});
+	});
 app.get('/resetpassword', routes.resetpassword);
 
 //Unused for now
